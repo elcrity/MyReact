ebf78555ecd7b5d99395f4a4ddce34485d4270a7
elcrity <elcrity@naver.com>
2023-08-17 17:50:24 +0900

    react-reduct, redux

---
+++ learn-redux/src/modules/index.js
@@ -0,0 +1,11 @@
+import counter from './counter'
+import todos from './todos'
+import { combineReducers} from 'redux'
+
+
+const rootReducer = combineReducers({
+  counter,
+  todos
+});
+
+export default rootReducer;
